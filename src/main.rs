2f942d44d263e1657759f98fb17e72f38f6084e2
Fay Carsons <faycarsons23@gmail.com>
2024-02-28 19:31:44 -0500

    added conversion of original at original size

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use clap::Parser;
 use serde::{Deserialize, Serialize};
 use serde_json::to_string_pretty;
 use std::collections::HashMap;
-
-use std::fs;
-use std::path::PathBuf;
+use std::ffi::OsStr;
+use std::fs::{self, DirEntry};
+use std::os::unix::ffi::OsStrExt;
+use std::path::{Path, PathBuf};
 
 type Title = String;
 
@@ -23,6 +24,11 @@ export type ImageSizing = {
 
 export type SizeMap = Record<string, ImageSizing>;";
 
+const TS_SIZEMAP_DECLARATION: &str = r"
+import type { SizeMap } from './image-types.ts'
+       
+export const {output_title}: SizeMap = ";
+
 #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
 struct Resolution {
     width: u32,
@@ -30,11 +36,11 @@ struct Resolution {
 }
 
 impl Resolution {
-    fn scale(&self, factor: u16) -> Self {
-        let factor = factor as f32 / 100.;
+    fn scale(self, factor: u16) -> Self {
+        let factor = f64::from(factor) / 100.;
         Self {
-            width: (self.width as f32 * factor) as u32,
-            height: (self.height as f32 * factor) as u32,
+            width: (f64::from(self.width) * factor) as u32,
+            height: (f64::from(self.height) * factor) as u32,
         }
     }
 
@@ -64,11 +70,9 @@ enum Scale {
 }
 
 impl Scale {
-    fn inner(&self) -> u16 {
+    fn inner(self) -> u16 {
         match self {
-            Self::Large(n) => *n,
-            Self::Medium(n) => *n,
-            Self::Small(n) => *n,
+            Self::Large(n) | Self::Medium(n) | Self::Small(n) => n,
         }
     }
 }
@@ -83,11 +87,11 @@ struct Args {
     #[arg(short, long)]
     output: PathBuf,
 
-    #[arg(long)]
+    #[arg(short, long)]
     js: PathBuf,
 
-    #[arg(short, long, value_parser = parse_scaling)]
-    scale: Option<Scaling>,
+    #[arg(short, long, default_value = "15 30 60", value_parser = parse_scaling)]
+    scale: Scaling,
 }
 
 fn parse_scaling(s: &str) -> Result<Scaling, String> {
@@ -107,6 +111,118 @@ fn parse_scaling(s: &str) -> Result<Scaling, String> {
     ])
 }
 
+fn is_image(s: &OsStr) -> bool {
+    matches!(s.as_bytes(), b"png" | b"jpg" | b"jpeg" | b"tiff" | b"raw")
+}
+
+fn convert_image(
+    file: &DirEntry,
+    scale: &Scaling,
+    output: &Path,
+) -> std::io::Result<(String, Image)> {
+    let path = file.path();
+    let path_str = path.to_str().expect("Error parsing file path: {path:?}");
+
+    // Get resolution
+    let get_resolution = std::process::Command::new("magick")
+        .args([
+            "identify",
+            "-ping",
+            "-format",
+            r#"{ "width": %w, "height": %h }"#,
+            path_str,
+        ])
+        .output()?;
+
+    let original = if get_resolution.status.success() {
+        let stdout = String::from_utf8_lossy(&get_resolution.stdout);
+        serde_json::from_str::<Resolution>(&stdout)
+    } else {
+        panic!("Image {path_str} cannot be read");
+    }?;
+
+    // Convert original to AVIF
+    if path
+        .extension()
+        .is_some_and(|ext| ext.as_bytes().to_ascii_lowercase() != b"avif")
+        && path.file_stem().is_some()
+    {
+        let output_path = path
+            .file_stem()
+            .and_then(|os| os.to_str())
+            .map(|s| s.to_string() + ".avif")
+            .unwrap();
+        let convert_avif = std::process::Command::new("magick")
+            .args(["convert", path_str, &output_path])
+            .output()?;
+
+        assert!(convert_avif.status.success());
+    }
+
+    let scale_processes = scale
+        .iter()
+        .map(|scl| {
+            let (tag, factor) = match scl {
+                Scale::Large(n) => ("_large", n),
+                Scale::Medium(n) => ("_medium", n),
+                Scale::Small(n) => ("_small", n),
+            };
+
+            let output_filename = path.file_stem().and_then(|s| s.to_str()).unwrap();
+            let output_filename = format!("{output_filename}{tag}.avif");
+            let output_path = output.join(output_filename.clone());
+            let output_path = output_path
+                .to_str()
+                .expect("Cannot convert `output_path` to str");
+
+            std::process::Command::new("magick")
+                .args([
+                    "convert",
+                    "-resize",
+                    &format!("{factor}%"),
+                    path_str,
+                    output_path,
+                ])
+                .spawn()
+        })
+        .collect::<std::io::Result<Vec<std::process::Child>>>()?;
+
+    path.file_stem()
+        .iter()
+        .for_each(|title| println!("Reszing image {title:?} and converting to AVIF"));
+
+    let scaled_results = scale_processes
+        .into_iter()
+        .map(std::process::Child::wait_with_output)
+        .collect::<Result<Vec<std::process::Output>, _>>()?;
+
+    if let Some(e) = scaled_results.into_iter().find(|p| !p.status.success()) {
+        eprintln!("Error creating scaled images: {e:?}");
+    };
+
+    let sizes = original.to_image(scale);
+
+    Ok((
+        path.file_stem()
+            .and_then(|s| s.to_str())
+            .unwrap()
+            .to_string(),
+        sizes,
+    ))
+}
+
+fn convert_dir(
+    dir: &Path,
+    scale: &Scaling,
+    output: &Path,
+) -> std::io::Result<HashMap<Title, Image>> {
+    dir.read_dir()?
+        .filter_map(Result::ok)
+        .filter(|entry| entry.path().extension().map_or(false, is_image))
+        .map(|ref entry| convert_image(entry, scale, output))
+        .collect::<std::io::Result<HashMap<Title, Image>>>()
+}
+
 fn main() -> std::io::Result<()> {
     let Args {
         input,
@@ -115,20 +231,15 @@ fn main() -> std::io::Result<()> {
         scale,
     } = Args::parse();
 
-    let scale = scale.unwrap_or([Scale::Small(15), Scale::Medium(30), Scale::Large(60)]);
+    let (input, output, js) = (&input, &output, &js);
 
-    let mut images = HashMap::<Title, Image>::new();
+    assert!(
+        (input.exists() && input.is_dir()),
+        "input directory {} does not exist!",
+        input.to_str().unwrap()
+    );
 
-    if !input.exists() || !input.is_dir() {
-        panic!(
-            "Input directory {} does not exist!",
-            input.to_str().unwrap()
-        );
-    }
-
-    if !output.exists() {
-        fs::create_dir(output.clone()).unwrap();
-    } else {
+    if output.exists() {
         let mut buffer = String::new();
         println!("Delete directory {:?}? [y/n]", output.clone());
         std::io::stdin()
@@ -137,94 +248,29 @@ fn main() -> std::io::Result<()> {
 
         if buffer.trim() == "y" {
             println!("Removing directory . . .");
-            fs::remove_dir_all(output.clone())?;
-            fs::create_dir(output.clone())?;
+            fs::remove_dir_all(output)?;
+            fs::create_dir(output)?;
         }
+    } else {
+        fs::create_dir(output)?;
     }
 
     if !js.exists() {
-        fs::create_dir(js.clone())?;
+        fs::create_dir(js)?;
     }
 
-    for file in input.read_dir().unwrap() {
-        let path = file?.path();
-        let path_str = path.to_str().unwrap();
-        println!("Path: {:?}", path);
-
-        let process = std::process::Command::new("magick")
-            .args([
-                "identify",
-                "-ping",
-                "-format",
-                r#"{ "width": %w, "height": %h }"#,
-                path_str,
-            ])
-            .output()?;
-
-        let original = if process.status.success() {
-            let stdout = String::from_utf8_lossy(&process.stdout);
-            serde_json::from_str::<Resolution>(&stdout)
-        } else {
-            panic!("Image: {path_str} cannot be read");
-        }
-        .expect("RESOLUTION DESERIALIZATION FAILED");
-
-        let scaled_images = scale
-            .into_iter()
-            .map(|scl| {
-                let (tag, factor) = match scl {
-                    Scale::Large(n) => ("_large", n),
-                    Scale::Medium(n) => ("_medium", n),
-                    Scale::Small(n) => ("_small", n),
-                };
-
-                let output_filename = path.file_stem().and_then(|s| s.to_str()).unwrap();
-                let output_filename = format!("{output_filename}{tag}.avif");
-                let output_path = output.join(output_filename.clone());
-                let output_path = output_path.to_str().expect("Cannot convert `output_path` to str");
-
-                println!("Path Str: {path_str}\n Output filename: {output_filename}\n Output path: {output_path}");
-
-                std::process::Command::new("magick")
-                    .args([
-                        "convert",
-                        "-resize",
-                        &format!("{factor}%"),
-                        path_str,
-                        output_path,
-                    ])
-                    .output()
-            })
-            .collect::<std::io::Result<Vec<std::process::Output>>>()?;
-
-        if let Some(e) = scaled_images.into_iter().find(|p| !p.status.success()) {
-            eprintln!("Error creating scaled images: {e:?}");
-        };
-
-        let sizes = original.to_image(&scale);
-
-        images.insert(
-            path.file_stem()
-                .and_then(|s| s.to_str())
-                .unwrap()
-                .to_string(),
-            sizes,
-        );
-    }
+    let size_map = convert_dir(input, &scale, output)?;
 
     let output_title = input
         .file_stem()
         .and_then(|s| s.to_str())
         .unwrap_or(output.file_stem().and_then(|s| s.to_str()).unwrap());
 
-    if !js.clone().join("image-types.ts").exists() {
+    if !js.join("image-types.ts").exists() {
         fs::write(js.join("image-types.ts"), TS_TYPE)?;
     }
 
-    let object = format!(
-        "const {output_title} = {};",
-        to_string_pretty(&images).unwrap()
-    );
+    let object = format!("{TS_SIZEMAP_DECLARATION}{};", to_string_pretty(&size_map)?);
     let output_title = &format!("{output_title}.ts");
     fs::write(js.join(output_title), object)
 }
